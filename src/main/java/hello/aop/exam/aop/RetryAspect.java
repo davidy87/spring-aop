fcab86c70438754781d211c61b38477d46346446
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-10-18 16:25:09 +0900

    Spring AOP - 프록시와 내부 호출 문제점 및 대안 예제 적용

--- src/main/java/hello/aop/exam/aop/RetryAspect.java
+++ src/main/java/hello/aop/exam/aop/RetryAspect.java
@@ -2,7 +2,6 @@ package hello.aop.exam.aop;
 
 import hello.aop.exam.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
