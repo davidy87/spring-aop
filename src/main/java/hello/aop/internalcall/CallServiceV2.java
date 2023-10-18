8b0ad0348fcca7b2998d504dc8169c81cac1ec6d
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-10-18 20:10:18 +0900

    Spring AOP - 프록시 기술과 한계 예제 적용

--- src/main/java/hello/aop/internalcall/CallServiceV2.java
+++ src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -1,20 +1,18 @@
 package hello.aop.internalcall;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class CallServiceV2 {
 
 //    private final ApplicationContext applicationContext;
 
     private final ObjectProvider<CallServiceV2> callServiceProvider;
-
-    public CallServiceV2(ObjectProvider<CallServiceV2> callServiceProvider) {
-        this.callServiceProvider = callServiceProvider;
-    }
 
     public void external() {
         log.info("call external");
